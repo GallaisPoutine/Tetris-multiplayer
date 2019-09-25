3fe3c9f1038ac4d3e5d12f6e08dbcabcb2daef59
GALLAIS-POU Raphaël <raphael.gallaispou@reseau.eseo.fr>
2019-09-25 21:22:37 +0200

    Add getter methods to field

--- src/field.rs
+++ src/field.rs
@@ -7,21 +7,46 @@ enum State {
 struct Field {
     u32 length,
     u32 depth,
-    u32 ** grid,
+    // TODO implement attribute "Matrix" grid
     State state,
     u32 numberOfLines,
     u32 level,
     u32 score,
 }
 
-fn FieldNew(length: u32, depth: u32) -> Field {
-    Field {
-        length: length,
-        depth: depth,
-        numberOfLines: 0,
-        level: 0,
-        score: 0,
-        // TODO define grid
-        // grid: 
+impl Field {
+    // Field constructor
+    fn new(length: u32, depth: u32) -> Field {
+        Field {
+            length: length,
+            depth: depth,
+            numberOfLines: 0,
+            level: 0,
+            score: 0,
+            // TODO initialize grid
+        }
     }
+
+    // Immutable access --> 
+    fn getLength(&self) -> &u32 {
+        &self.length
+    }
+    fn getDepth(&self) -> &u32 {
+        &self.depth
+    }
+    // TODO implement getter for grid
+    fn getState(&self) -> &State {
+        &self.state
+    }
+    fn getNumberOfLines(&self) -> &u32 {
+        &self.numberOfLines
+    }
+    fn getLevel(&self) -> &u32 {
+        &self.level
+    }
+    fn getScore(&self) -> &u32 {
+        &self.score
+    }
+
+    // TODO implement Setters
 }
