ff1d86d383b18c5b7a8f43ab6555076ba823b71a
GALLAIS-POU Raphaël <raphael.gallaispou@reseau.eseo.fr>
2019-09-30 13:33:04 +0200

    Implement function in field + grid atrtibute

--- src/field.rs
+++ src/field.rs
@@ -1,42 +1,44 @@
 
-enum State {
-    EMPTY,
-    FULL,
-}
+// mod tetromino;
+//  COmming from tetromino, remove after
+const TETROMINO_LENGTH: u32 = 4;
+const LENGTH_OF_NEXT_LIST: u32 = 7;
 
 struct Field {
-    u32 length,
-    u32 depth,
-    // TODO implement attribute "Matrix" grid
-    State state,
-    u32 numberOfLines,
-    u32 level,
-    u32 score,
+    length: usize,
+    depth: usize,
+    grid: [[u32; 24]; 10],
+    empty: bool,
+    numberOfLines: u32,
+    level: u32,
+    score: u32,
 }
 
 impl Field {
+
     // Field constructor
-    fn new(length: u32, depth: u32) -> Field {
+    fn new(length: usize, depth: usize) -> Field {
         Field {
             length: length,
             depth: depth,
+            grid: [[0; 24]; 10],
+            empty:false,
             numberOfLines: 0,
             level: 0,
             score: 0,
-            // TODO initialize grid
         }
     }
 
     // Immutable access --> 
-    fn getLength(&self) -> &u32 {
+    fn getLength(&self) -> &usize {
         &self.length
     }
-    fn getDepth(&self) -> &u32 {
+    fn getDepth(&self) -> &usize {
         &self.depth
     }
     // TODO implement getter for grid
-    fn getState(&self) -> &State {
-        &self.state
+    fn getState(&self) -> &bool {
+        &self.empty
     }
     fn getNumberOfLines(&self) -> &u32 {
         &self.numberOfLines
@@ -49,4 +51,25 @@ impl Field {
     }
 
     // TODO implement Setters
+
+//  Determine si le joueur a perdu
+    fn isInLossZone(&self) -> bool {
+        println!("IS IN LOSS ZONE\n");
+        let mut loss = false;
+        let mut i: usize = 0;
+        while i<4 && !loss {
+            let mut j:usize = 0;
+            while j<self.depth && !loss {
+                if self.grid[i][j] != 0    {
+                    loss = true;
+                    self.empty = true;
+                }
+                j+= 1;
+            }
+            i+= 1;
+        }
+        loss
+    }
+
+
 }
